cabb7ca273b64f6196e0dedecb4ad30b50ef77ce
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-03-16 23:59:06 +0000

    Added service method tests.

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -12,14 +12,18 @@ import (
 )
 
 const (
-	emailTest = "email@domain.com"
+	idTest    = "00000000-0000-0000-0000-000000000000"
+	emailTest = "email@domain"
+	nameTest  = "Test"
 )
 
-func TestService_GetUserByEmail(t *testing.T) {
-	userTest := model.User{
-		Email: emailTest,
-	}
+var userTest = model.User{
+	Id:    idTest,
+	Name:  nameTest,
+	Email: emailTest,
+}
 
+func TestService_FindUser(t *testing.T) {
 	t.Run("Returns an error", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		defer ctrl.Finish()
@@ -29,11 +33,11 @@ func TestService_GetUserByEmail(t *testing.T) {
 		service := NewService(repositoryMock)
 
 		repositoryMock.EXPECT().
-			FindUser(gomock.Any(), emailTest).
+			FindUser(gomock.Any(), idTest).
 			Return(model.User{}, fmt.Errorf("error")).
 			Times(1)
 
-		_, err := service.FindUser(context.Background(), emailTest)
+		_, err := service.FindUser(context.Background(), idTest)
 		assert.Error(t, err)
 	})
 
@@ -46,12 +50,155 @@ func TestService_GetUserByEmail(t *testing.T) {
 		service := NewService(repositoryMock)
 
 		repositoryMock.EXPECT().
-			FindUser(gomock.Any(), emailTest).
+			FindUser(gomock.Any(), idTest).
 			Return(userTest, nil).
 			Times(1)
 
-		user, err := service.FindUser(context.Background(), emailTest)
+		user, err := service.FindUser(context.Background(), idTest)
 		assert.NoError(t, err)
 		assert.Equal(t, userTest, user)
+	})
+}
+
+func TestService_CreateUser(t *testing.T) {
+	t.Run("Returns an error ", func(t *testing.T) {
+		t.Run("in create user method.", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repositoryMock := NewMockRepository(ctrl)
+
+			service := NewService(repositoryMock)
+
+			gomock.InOrder(
+				repositoryMock.EXPECT().
+					CreateUser(gomock.Any(), userTest).
+					Return(idTest, fmt.Errorf("error")).
+					Times(1),
+			)
+
+			_, err := service.CreateUser(context.Background(), userTest)
+			assert.Error(t, err)
+		})
+
+		t.Run("in find user method", func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			repositoryMock := NewMockRepository(ctrl)
+
+			service := NewService(repositoryMock)
+
+			gomock.InOrder(
+				repositoryMock.EXPECT().
+					CreateUser(gomock.Any(), userTest).
+					Return(idTest, nil).
+					Times(1),
+				repositoryMock.EXPECT().
+					FindUser(gomock.Any(), idTest).
+					Return(model.User{}, fmt.Errorf("error")).
+					Times(1),
+			)
+
+			_, err := service.CreateUser(context.Background(), userTest)
+			assert.Error(t, err)
+		})
+	})
+
+	t.Run("Returns no error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		repositoryMock := NewMockRepository(ctrl)
+
+		service := NewService(repositoryMock)
+
+		gomock.InOrder(
+			repositoryMock.EXPECT().
+				CreateUser(gomock.Any(), userTest).
+				Return(idTest, nil).
+				Times(1),
+			repositoryMock.EXPECT().
+				FindUser(gomock.Any(), idTest).
+				Return(userTest, nil).
+				Times(1),
+		)
+
+		user, err := service.CreateUser(context.Background(), userTest)
+		assert.NoError(t, err)
+		assert.Equal(t, userTest, user)
+	})
+}
+
+func TestService_UpdateUser(t *testing.T) {
+	t.Run("Returns an error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		repositoryMock := NewMockRepository(ctrl)
+
+		service := NewService(repositoryMock)
+
+		repositoryMock.EXPECT().
+			UpdateUser(gomock.Any(), userTest).
+			Return(fmt.Errorf("error")).
+			Times(1)
+
+		_, err := service.UpdateUser(context.Background(), userTest)
+		assert.Error(t, err)
+	})
+
+	t.Run("Returns no error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		repositoryMock := NewMockRepository(ctrl)
+
+		service := NewService(repositoryMock)
+
+		repositoryMock.EXPECT().
+			UpdateUser(gomock.Any(), userTest).
+			Return(nil).
+			Times(1)
+
+		user, err := service.UpdateUser(context.Background(), userTest)
+		assert.NoError(t, err)
+		assert.Equal(t, userTest, user)
+	})
+}
+
+func TestService_DeleteUser(t *testing.T) {
+	t.Run("Returns an error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		repositoryMock := NewMockRepository(ctrl)
+
+		service := NewService(repositoryMock)
+
+		repositoryMock.EXPECT().
+			DeleteUser(gomock.Any(), idTest).
+			Return(fmt.Errorf("error")).
+			Times(1)
+
+		err := service.DeleteUser(context.Background(), idTest)
+		assert.Error(t, err)
+	})
+
+	t.Run("Returns no error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		repositoryMock := NewMockRepository(ctrl)
+
+		service := NewService(repositoryMock)
+
+		repositoryMock.EXPECT().
+			DeleteUser(gomock.Any(), idTest).
+			Return(nil).
+			Times(1)
+
+		err := service.DeleteUser(context.Background(), idTest)
+		assert.NoError(t, err)
 	})
 }
