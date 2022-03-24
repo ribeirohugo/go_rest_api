0124566d60a2fcc6b2f9ea80b9862c95c2ffc729
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-03-24 11:52:56 +0000

    Revert "Added bson fields to user model."
    This reverts commit c9e98d18

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	Id    string `json,bson:"id"`
-	Name  string `json,bson:"name"`
-	Email string `json,bson:"email"`
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
