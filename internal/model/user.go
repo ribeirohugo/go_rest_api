dd1a72905edd516d3a7f511b2388bf193f485434
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-03-24 12:10:44 +0000

    Fixed json/bson parse in user model.

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,6 +2,6 @@ package model
 
 type User struct {
 	Id    string `bson:"_id,omitempty" json:"id,omitempty"`
-	Name  string `bson:"username,omitempty" json:"username,omitempty"`
+	Name  string `bson:"name,omitempty" json:"name,omitempty"`
 	Email string `bson:"email,omitempty" json:"email,omitempty"`
 }
