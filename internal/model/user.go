1ba9b1d4210fb74112a213f04d0d7a894846b5bd
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-03-24 12:02:11 +0000

    Fixed json parse bug.

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	Id    string `bson:"_id,omitempty" json:"_id,omitempty"`
+	Id    string `bson:"_id,omitempty" json:"id,omitempty"`
 	Name  string `bson:"username,omitempty" json:"username,omitempty"`
 	Email string `bson:"email,omitempty" json:"email,omitempty"`
 }
