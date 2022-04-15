0ff68abaf7cceda0b0be5fea81c70baf1dd63a7e
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-04-15 18:20:35 +0100

    Updated user model struct with timestamp fields.

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,9 +1,13 @@
 // Package model holds domain model structs and methods.
 package model
 
+import "time"
+
 // User - domain model for a user
 type User struct {
-	ID    string `bson:"_id,omitempty" json:"id,omitempty"`
-	Name  string `bson:"name,omitempty" json:"name,omitempty"`
-	Email string `bson:"email,omitempty" json:"email,omitempty"`
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name,omitempty" json:"name,omitempty"`
+	Email     string    `bson:"email,omitempty" json:"email,omitempty"`
+	CreatedAt time.Time `bson:"createdAt,omitempty" json:"createdAt,omitempty"`
+	UpdatedAt time.Time `bson:"updatedAt,omitempty" json:"updatedAt,omitempty"`
 }
