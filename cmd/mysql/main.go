493749fe678fad4d4a95754dab9140de2ab95380
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-04-15 17:52:59 +0100

    Added MySQL cmd main.

---
+++ cmd/mysql/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ribeirohugo/golang_startup/internal/config"
+	"github.com/ribeirohugo/golang_startup/internal/controller"
+	"github.com/ribeirohugo/golang_startup/internal/database/mysql"
+	"github.com/ribeirohugo/golang_startup/internal/server"
+	"github.com/ribeirohugo/golang_startup/internal/service"
+)
+
+func main() {
+	cfg := config.Load()
+
+	database, err := mysql.New(cfg.Database.Address)
+	if err != nil {
+		log.Fatalf("failed to initialise the database client: %v", err)
+	}
+
+	err = database.Migrate(cfg.Database.Name, cfg.Database.MigrationsPath)
+	if err != nil {
+		log.Fatalf("failed initialise database migrations: %v", err)
+	}
+
+	services := service.New(database)
+
+	controllers := controller.New(services)
+
+	srv := server.New(controllers, cfg.GetServerAddress())
+
+	srv.ServeHTTP()
+}
